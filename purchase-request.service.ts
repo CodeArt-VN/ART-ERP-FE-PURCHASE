f062807fbdf5cb02f5b176f2ed92d267c8e5a53b
tanphuc <bizyoh@gmail.com>
2025-02-27 14:54:56 +0700

    up

--- purchase-request.service.ts
+++ purchase-request.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-
+import { ApiSetting } from 'src/app/services/static/api-setting';
 import { PURCHASE_RequestProvider } from 'src/app/services/static/services.service';
 import { EnvService } from 'src/app/services/core/env.service';
 import { CommonService } from 'src/app/services/core/common.service';
@@ -24,38 +24,30 @@ export class PURCHASE_RequestService extends PURCHASE_RequestProvider {
 		{ Status: 'Merged', ShowBtns: ['ShowArchive'] }, // Đơn đã gộp
 		{ Status: 'Canceled', ShowBtns: ['ShowArchive'] }, // Đơn đã gộp
 	];
-	sendRequestQuotationToVendor(id, orderLines, currentVendor, componentModal, modalController, env) {
-		return new Promise(async (resolve, reject) => {
-			const modal = await modalController.create({
-				component: componentModal,
-				componentProps: {
-					itemInVendors: orderLines,
-					isMultiple: true,
-					defaultVendor: currentVendor,
-					// vendorList : vendorList,
-				},
 
-				cssClass: 'modal90',
-			});
-
-			await modal.present();
-			const { data } = await modal.onWillDismiss();
-			if (data && data.some((d) => d.Vendors.length > 0)) {
-				let postData = { data: data.filter((d) => d.Vendors.length > 0) };
-				env.showLoading('Please wait for a few moments', this.commonService.connect('POST', 'PURCHASE/Request/SendRequestQuotationToVendor/' + id, postData).toPromise())
-					.then((resp: any) => {
-						if (resp) {
-							env.showMessage('Purchase quotations created!', 'success');
-							resolve(resp);
-						}
-					})
-					.catch((err) => {
-						console.log(err);
-						env.showMessage('Cannot create PQ, please try again later', 'danger');
-						reject(err);
-					});
+	copyToPO(items: any, env, pageConfig) {
+		return new Promise((resolve, reject) => {
+			let postDTO = { Ids: [] };
+			let length = 0;
+			let itemName = '';
+			if (Array.isArray(items)) {
+				postDTO.Ids = items.map((item: any) => item.Id);
+				length = items.length;
+			} else {
+				postDTO.Ids = [items.Id];
+				itemName = items.Name;
 			}
-			else resolve(null);
+			env.actionConfirm('submit', length, items?.Name, pageConfig.pageTitle, () =>
+				this.commonService.connect('POST', ApiSetting.apiDomain('PURCHASE/Request/CopyToPO/'), postDTO).toPromise()
+			)
+				.then((savedItem: any) => {
+					env.publishEvent({ Code: pageConfig.pageName });
+					env.showMessage('Purchased ordered', 'success');
+					resolve(savedItem);
+				})
+				.catch((err) => {
+					reject(err);
+				});
 		});
 	}
 }
