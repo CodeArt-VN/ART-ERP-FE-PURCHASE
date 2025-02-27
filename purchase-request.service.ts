2e3dab8be800b14164de7c6ccb39601d8bc24a3e
tanphuc <bizyoh@gmail.com>
2025-02-27 14:56:08 +0700

    update service

--- purchase-request.service.ts
+++ purchase-request.service.ts
@@ -1,13 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { ApiSetting } from 'src/app/services/static/api-setting';
 import { PURCHASE_RequestProvider } from 'src/app/services/static/services.service';
-import { EnvService } from 'src/app/services/core/env.service';
-import { CommonService } from 'src/app/services/core/common.service';
-import { LoadingController, ModalController, NavController } from '@ionic/angular';
-import { PurchaseOrderModalPage } from './purchase-request-detail/purchase-order-modal/purchase-order-modal.page';
-import { PurchaseQuotationModalPage } from './purchase-request-detail/purchase-quotation-modal/purchase-quotation-modal.page';
+
 
 @Injectable({ providedIn: 'root' })
 export class PURCHASE_RequestService extends PURCHASE_RequestProvider {
@@ -38,7 +32,7 @@ export class PURCHASE_RequestService extends PURCHASE_RequestProvider {
 				itemName = items.Name;
 			}
 			env.actionConfirm('submit', length, items?.Name, pageConfig.pageTitle, () =>
-				this.commonService.connect('POST', ApiSetting.apiDomain('PURCHASE/Request/CopyToPO/'), postDTO).toPromise()
+				this.commonService.connect('POST','PURCHASE/Request/CopyToPO/', postDTO).toPromise()
 			)
 				.then((savedItem: any) => {
 					env.publishEvent({ Code: pageConfig.pageName });
@@ -48,6 +42,40 @@ export class PURCHASE_RequestService extends PURCHASE_RequestProvider {
 				.catch((err) => {
 					reject(err);
 				});
+		});
+	}
+	sendRequestQuotationToVendor(id, orderLines, currentVendor, componentModal, modalController, env) {
+		return new Promise(async (resolve, reject) => {
+			const modal = await modalController.create({
+				component: componentModal,
+				componentProps: {
+					itemInVendors: orderLines,
+					isMultiple: true,
+					defaultVendor: currentVendor,
+					// vendorList : vendorList,
+				},
+
+				cssClass: 'modal90',
+			});
+
+			await modal.present();
+			const { data } = await modal.onWillDismiss();
+			if (data && data.some((d) => d.Vendors.length > 0)) {
+				let postData = { data: data.filter((d) => d.Vendors.length > 0) };
+				env.showLoading('Please wait for a few moments', this.commonService.connect('POST', 'PURCHASE/Request/SendRequestQuotationToVendor/' + id, postData).toPromise())
+					.then((resp: any) => {
+						if (resp) {
+							env.showMessage('Purchase quotations created!', 'success');
+							resolve(resp);
+						}
+					})
+					.catch((err) => {
+						console.log(err);
+						env.showMessage('Cannot create PQ, please try again later', 'danger');
+						reject(err);
+					});
+			}
+			else resolve(null);
 		});
 	}
 }
