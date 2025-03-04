c102bbc13ec225e1a0bc85d2dbc50a98bd73bfb9
Hùng Vũ <hungvq@live.com>
2025-03-04 18:09:27 +0700

    move showCommandRules to toolbar rules

--- purchase-request.service.ts
+++ purchase-request.service.ts
@@ -3,19 +3,6 @@ import { PURCHASE_RequestProvider } from 'src/app/services/static/services.servi
 
 @Injectable({ providedIn: 'root' })
 export class PURCHASE_RequestService extends PURCHASE_RequestProvider {
-	showCommandRules = [
-		{ Status: 'Draft', ShowBtns: ['ShowChangeBranch', 'ShowSubmit', 'ShowApprove', 'ShowCancel', 'ShowDelete', 'ShowArchive'] }, // Mới
-		{ Status: 'Unapproved', ShowBtns: ['ShowChangeBranch', 'ShowSubmit', 'ShowApprove', 'ShowCancel', 'ShowDelete', 'ShowArchive'] }, // Không duyệt
-		{ Status: 'Submitted', ShowBtns: ['ShowApprove', 'ShowDisapprove', 'ShowCancel', 'ShowDelete', 'ShowArchive'] }, // Chờ duyệt
-		{ Status: 'Approved', ShowBtns: ['ShowDisapprove', 'ShowCancel', 'ShowArchive', 'ShowCopyToPurchaseQuotation', 'ShowCopyToPO'] }, // Đã duyệt
-
-		{ Status: 'Open', ShowBtns: ['ShowArchive'] }, // Đang xử lý
-		{ Status: 'Closed', ShowBtns: ['ShowArchive'] }, // Đã xong
-
-		{ Status: 'Splitted', ShowBtns: ['ShowArchive'] }, // Đơn đã chia
-		{ Status: 'Merged', ShowBtns: ['ShowArchive'] }, // Đơn đã gộp
-		{ Status: 'Canceled', ShowBtns: ['ShowArchive'] }, // Đơn đã gộp
-	];
 
 	copyToPO(id, orderLines: any, currentVendor, vendorList, modalComponent, modalController, env) {
 		return new Promise(async (resolve, reject) => {
